e1d21cbd5ac012047d4d0ae24c1f8926bf0c8cfa
012xx <night.fox.045@gmail.com>
2022-05-03 02:32:44 +0900

    Add:lesson04

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 "use strict";
 
-for (let a = 1; a <= 9; a += 1) {
-  for (let b = 1; b <= 9; b += 1) {
-    console.log(`${a}*${b} = ${a * b}`);
-  }
-}
+// index.htmlからmainを引っ張ってくる
+// p要素の作成→代入
+// ブラウザに表示
+
+const element = document.getElementById("top");
+console.log(element);
+const p_text = document.createElement("p");
+p_text.textContent = "Hello World!!";
+console.log(p_text);
+
+document.main.appendChild(p_text);

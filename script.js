4d41833fa3927754cdfa6cfee12df9389986e4b3
012xx <night.fox.045@gmail.com>
2022-05-03 16:34:51 +0900

    Add:use-appendChild

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 "use strict";
 
 // index.htmlからmainを引っ張ってくる
-// p要素の作成→代入
-// ブラウザに表示
-
 const element = document.getElementById("top");
 console.log(element);
+
+// p要素の作成→代入
 const p_text = document.createElement("p");
 p_text.textContent = "Hello World!!";
 console.log(p_text);
 
-document.main.appendChild(p_text);
+// ブラウザに表示
+element.appendChild(p_text);

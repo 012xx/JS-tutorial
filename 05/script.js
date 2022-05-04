6c56698c080a8dfca21c913d3026e4a9438f6332
012xx <night.fox.045@gmail.com>
2022-05-04 18:20:25 +0900

    Add:use array

--- 05/script.js
+++ 05/script.js
@@ -1,8 +1,12 @@
 "use strict";
 
 const array = [1, 2, 3];
-for (let el of array) {
-  el = "Hello World!";
-  console.log(el);
-  console.log(array);
-}
+console.log(array);
+
+array.push(4);
+console.log(array);
+array.unshift(0);
+console.log(array);
+array.pop(); // 末尾の要素を削除
+console.log(array);
+array.shift(); // 銭湯の要素を削除

82caf9afb5aeb3bba4db19310886779009f55577
012xx <night.fox.045@gmail.com>
2022-05-04 18:50:09 +0900

    Add:use array let

--- 05/script.js
+++ 05/script.js
@@ -1,12 +1,9 @@
 "use strict";
 
 const array = [1, 2, 3];
-console.log(array);
 
-array.push(4);
-console.log(array);
-array.unshift(0);
-console.log(array);
-array.pop(); // 末尾の要素を削除
-console.log(array);
-array.shift(); // 銭湯の要素を削除
+const tail = array.pop(); // 末尾削除
+console.log(tail);
+
+let head = array.shift(); // 先頭を削除
+console.log(head);

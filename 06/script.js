fe2aa73590014182ed71b830c911405eab0490af
012xx <night.fox.045@gmail.com>
2022-05-14 10:01:57 +0900

    use for in

--- 06/script.js
+++ 06/script.js
@@ -3,10 +3,19 @@
 const obj = { a: "A", b: "B", c: "C" };
 
 // プロパティ名 a を直接指定する
-console.log(obj.a); // → A (ドット記法)
-console.log(obj["a"]); // → A (ブラケット記法)
+// console.log(obj.a); // → A (ドット記法)
+// console.log(obj["a"]); // → A (ブラケット記法)
 
 // プロパティ名 a を定数に代入してから指定する
-const prop = "a";
-console.log(obj.prop); // → undefined
-console.log(obj[prop]); // → A
+// const prop = "a";
+// console.log(obj.prop); // → undefined
+// console.log(obj[prop]); // → A
+
+// for (const el of obj) {
+//   console.log(el);
+// }
+
+for (const prop in obj) {
+  console.log(prop);
+  console.log(obj[prop]);
+}

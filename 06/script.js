ce15f2d4661c368453bd5464c4e20a06a67394f6
012xx <night.fox.045@gmail.com>
2022-05-14 10:06:55 +0900

    use Object.entries

--- 06/script.js
+++ 06/script.js
@@ -2,20 +2,7 @@
 
 const obj = { a: "A", b: "B", c: "C" };
 
-// プロパティ名 a を直接指定する
-// console.log(obj.a); // → A (ドット記法)
-// console.log(obj["a"]); // → A (ブラケット記法)
-
-// プロパティ名 a を定数に代入してから指定する
-// const prop = "a";
-// console.log(obj.prop); // → undefined
-// console.log(obj[prop]); // → A
-
-// for (const el of obj) {
-//   console.log(el);
-// }
-
-for (const prop in obj) {
+for (const [prop, val] of Object.entries(obj)) {
   console.log(prop);
-  console.log(obj[prop]);
+  console.log(val);
 }

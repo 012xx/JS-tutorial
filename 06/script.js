d76459ec15f23b6f0f3543effc6abdeb1c0a21ae
012xx <night.fox.045@gmail.com>
2022-05-14 14:28:13 +0900

    use Object.entries

--- 06/script.js
+++ 06/script.js
@@ -1,8 +1,29 @@
 "use strict";
 
-const obj = { a: "A", b: "B", c: "C" };
+const profile = {
+  name: "OJK",
+  bloodType: "A",
+  servants: ["犬", "猿", "雉"],
+  style: { height: 165, weight: 52, waist: 76 },
+};
 
-for (const [prop, val] of Object.entries(obj)) {
-  console.log(prop);
-  console.log(val);
+console.log(profile.servants[2]);
+console.log(profile.style.height);
+
+// profile.servantsは単なる配列
+for (const servant of profile.servants) {
+  console.log(`家来：${servant}`);
+}
+
+// profile.styleは単なるオブジェクト
+const myStyle = profile.style;
+for (const prop in myStyle) {
+  console.log(`${prop} is ${myStyle[prop]}`);
+}
+
+// Object.entriesを使ってfor-of文で書く
+for (const [prop, val] of Object.entries(profile)) {
+  if (prop == "name" || prop == "bloodType") {
+    console.log(`${prop}は${val}です`);
+  }
 }

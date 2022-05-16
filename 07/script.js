52977476cda5936171942a8ec84eef7bf4b3b5c2
012xx <night.fox.045@gmail.com>
2022-05-16 18:55:01 +0900

    Add:servants

--- 07/script.js
+++ 07/script.js
@@ -7,3 +7,21 @@ for (const servant of servants) {
   console.log(servant);
 }
 console.log(`の総勢${servants.length}名です。`);
+
+// 「鬼」を配列の末尾に追加
+servants.push("鬼");
+
+console.log("現在の家来は：");
+for (const servant of servants) {
+  console.log(servant);
+}
+console.log(`の総勢${servants.length}名です。`);
+
+// 「桃」を配列の末尾に追加
+servants.push("桃");
+
+console.log("現在の家来は：");
+for (const servant of servants) {
+  console.log(servant);
+}
+console.log(`の総勢${servants.length}名です。`);

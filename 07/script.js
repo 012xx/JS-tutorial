d4c9d4c2f080816a35483a8780c66a2e1360c816
012xx <night.fox.045@gmail.com>
2022-05-16 19:05:17 +0900

    Use:function

--- 07/script.js
+++ 07/script.js
@@ -2,26 +2,18 @@
 
 const servants = ["犬", "猿", "雉"];
 
-console.log("現在の家来は：");
-for (const servant of servants) {
-  console.log(servant);
+function showServants() {
+  console.log("現在の家来は：");
+  for (const servant of servants) {
+    console.log(servant);
+  }
+  console.log(`の総勢${servants.length}名です。`);
 }
-console.log(`の総勢${servants.length}名です。`);
 
-// 「鬼」を配列の末尾に追加
+showServants();
+
 servants.push("鬼");
+showServants();
 
-console.log("現在の家来は：");
-for (const servant of servants) {
-  console.log(servant);
-}
-console.log(`の総勢${servants.length}名です。`);
-
-// 「桃」を配列の末尾に追加
 servants.push("桃");
-
-console.log("現在の家来は：");
-for (const servant of servants) {
-  console.log(servant);
-}
-console.log(`の総勢${servants.length}名です。`);
+showServants();

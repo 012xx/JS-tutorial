560b68c1686a893d5d76bbbe535cbbb3cc345408
012xx <night.fox.045@gmail.com>
2022-05-16 18:46:29 +0900

    no-arguments and return values

---
+++ 07/script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const servants = ["犬", "猿", "雉"];
+
+console.log("現在の家来は：");
+for (const servant of servants) {
+  console.log(servant);
+}
+console.log(`の総勢${servants.length}名です。`);

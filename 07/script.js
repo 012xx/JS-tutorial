3bd0416e96034e7de6dd5b7b547dea939f1bb4a3
012xx <night.fox.045@gmail.com>
2022-05-17 16:29:01 +0900

    Use:return value

--- 07/script.js
+++ 07/script.js
@@ -2,17 +2,18 @@
 
 const servants = ["犬", "猿", "雉"];
 
-function appendServants(newcomer1, newcomer2) {
-  servants.push(newcomer1);
-  servants.push(newcomer2);
+function swapServants(newcomer) {
+  servants.push(newcomer);
+  const fired = servants.shift(); // 先頭要素を削除
 
   console.log("現在の家来は：");
   for (const servant of servants) {
     console.log(servant);
   }
   console.log(`の総勢${servants.length}名です。`);
+
+  return fired;
 }
 
-appendServants("赤鬼", "青鬼");
-appendServants("桃", "栗");
-appendServants("爺", "婆");
+console.log(`${swapServants("鬼")}さん、これまでありがとう！`);
+console.log(`${swapServants("桃")}さん、これまでありがとう！`);

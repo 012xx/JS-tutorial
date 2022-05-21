aa3135db802208d47e7602975e69b425ef0e82bf
012xx <night.fox.045@gmail.com>
2022-05-21 19:02:51 +0900

    Use:NodeList

--- 08/script.js
+++ 08/script.js
@@ -1,3 +1,8 @@
 "use strict";
 
-console.log("Hello World!!");
+const lis = document.querySelectorAll("li");
+console.log(lis); // → NodeList(3) [li,li,li]
+
+console.log(lis[0].textContent);
+console.log(lis[1].textContent);
+console.log(lis[2].textContent);

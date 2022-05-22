a8df0dbdba381abd56111f58c9b676df716f5d3f
012xx <night.fox.045@gmail.com>
2022-05-22 16:44:46 +0900

    Use:getElementsByClassName

--- 08/script.js
+++ 08/script.js
@@ -1,15 +1,4 @@
 "use strict";
 
-const lis = document.getElementsByTagName("li");
-console.log(lis); // → HTMLCollection(3) [li, li, li]
-
-for (const li of lis) {
-  console.log(li.textContent);
-}
-
-const lisArray = Array.from(lis); // 配列に変換
-lisArray.forEach((li) => (li.textContent = "DOM!!"));
-
-for (const li of lis) {
-  console.log(li.textContent);
-}
+const ps = document.getElementsByClassName("explain");
+console.log(ps);

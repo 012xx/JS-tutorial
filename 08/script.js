f332137899555cf2316da62bf567e1c8c05e638d
012xx <night.fox.045@gmail.com>
2022-05-22 17:22:19 +0900

    How:Elements

--- 08/script.js
+++ 08/script.js
@@ -1,4 +1,10 @@
 "use strict";
 
-const inputs = document.getElementsByName("dom");
-console.log(inputs);
+const a = document.getElementById("link"); // a要素の取得
+console.log(a); // a要素の表示
+
+const p = a.parentElement; // 親要素
+console.log(p); // aの親要素の表示
+
+const img = a.firstElementChild; // 最初の親要素
+console.log(img); //

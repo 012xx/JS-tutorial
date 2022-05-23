13f2108a55064f3f52f7806a22469d9332c353c0
012xx <night.fox.045@gmail.com>
2022-05-23 16:42:56 +0900

    Replaced the image

--- 09/script.js
+++ 09/script.js
@@ -1,8 +1,19 @@
 "use strict";
 
-// 定数pにはElementByIdで取ってきたID=p_idを入れる
-const p = document.getElementById("p_id");
+// ① a要素とimg要素のエレメントを取得
+// 定数a : getElementByIdでリンクを取得
+const a = document.getElementById("link");
+// 定数img : aの最初の子要素を取得
+const img = a.firstElementChild;
 
-// 定数p_idを表示する
-console.log(`p.class:${p.class}`);
-console.log(`p.className:${p.className}`);
+// ② 属性値を書き換えることもできる
+// 元のリンクURLを表示
+console.log(`元のリンクURL:${a.href}`);
+// リンクを'https://loremflickr.com/320/240/dog'に変更する
+a.href = "https://loremflickr.com/320/240/dog";
+
+// ③だから、画像を書き換えることもできる
+// 元の画像ソースを表示
+console.log(`元の画像ソース:${img.href}`);
+// 画像も同様に書き換える
+img.src = "https://loremflickr.com/320/240/dog";

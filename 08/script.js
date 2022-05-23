0d9643ffa34eaeba0011b66242ec93e8fb316c1c
012xx <night.fox.045@gmail.com>
2022-05-23 12:56:00 +0900

    Use:replaceChild

--- 08/script.js
+++ 08/script.js
@@ -1,9 +1,8 @@
 "use strict";
 
 const ul = document.getElementById("list");
+const li1 = ul.firstElementChild; // 1番目の子要素
+const newLi = document.createElement("li"); // 新しいli要素
+newLi.textContent = "DOM!!";
 
-const li1 = ul.firstElementChild; // 1番目の要素
-const li2 = li1.nextElementSibling; // 2番目の要素
-const li3 = li2.nextElementSibling; // 3番目の要素
-
-ul.insertBefore(li3, li2);
+ul.replaceChild(newLi, li1); // 1番目の子要素と新しい要素を置換

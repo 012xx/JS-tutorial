3f57b4764db94c4801aecd6a8e6b156cf9ca9fd6
012xx <night.fox.045@gmail.com>
2022-05-23 20:53:45 +0900

    JavaScript で class を切り替え

--- 09/script.js
+++ 09/script.js
@@ -1,12 +1,6 @@
 "use strict";
 
-const a = document.getElementById("link");
+const p = document.getElementById("p_id");
+console.log(p.className); // → p_class
 
-const attrs = a.attributes; // → 属性一覧を取得
-
-console.log(attrs.href.name); // → href
-console.log(attrs.href.value); // → "https://zenn.dev/ojk"
-
-// value(属性値)の方は書き換えも可能
-attrs.href.value = "https://loremflickr.com/320/240/dog";
-console.log(attrs.href.value);
+p.className = "myStyle"; // myStyleクラスに切り替え

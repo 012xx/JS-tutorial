6ea758e67a8855fb1ee1b950828c160aab528225
012xx <night.fox.045@gmail.com>
2022-05-23 11:38:12 +0900

    Use:nextElementSibling

--- 08/script.js
+++ 08/script.js
@@ -1,6 +1,12 @@
 "use strict";
 
-const ul = document.querySelector("ul");
-console.log(ul.childNodes);
-// → NodeList(7) [text,li,text,li,text]
-// 改行文字がテキストノードとしてli要素の間に含まれる
+const ul = document.getElementById("list"); // 親要素 → ul
+
+const li1 = ul.firstElementChild; //  最初の親要素 → li1
+console.log(li1.textContent); // → D … Document
+
+const li2 = li1.nextElementSibling; // li1の次の兄弟要素 → li2
+console.log(li2.textContent); // → O … Object
+
+const li3 = li2.nextElementSibling; // li2の次の兄弟要素 → li3
+console.log(li3.textContent); // → M … Model

cf640441326956e1522076436b62cc71e9277ead
012xx <night.fox.045@gmail.com>
2022-05-23 20:42:34 +0900

    属性名と属性値の取り出し && 属性書き換え

--- 09/script.js
+++ 09/script.js
@@ -4,7 +4,9 @@ const a = document.getElementById("link");
 
 const attrs = a.attributes; // → 属性一覧を取得
 
-// for-ofが使える
-for (const attr of attrs) {
-  console.log(attr); // → id="link" → ...
-}
+console.log(attrs.href.name); // → href
+console.log(attrs.href.value); // → "https://zenn.dev/ojk"
+
+// value(属性値)の方は書き換えも可能
+attrs.href.value = "https://loremflickr.com/320/240/dog";
+console.log(attrs.href.value);

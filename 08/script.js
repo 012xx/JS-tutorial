389702969c13b57d486c4a85dcc4ea8eb2b2332d
012xx <night.fox.045@gmail.com>
2022-05-23 11:42:58 +0900

    Use:previousElementSibling

--- 08/script.js
+++ 08/script.js
@@ -2,11 +2,11 @@
 
 const ul = document.getElementById("list"); // 親要素 → ul
 
-const li1 = ul.firstElementChild; //  最初の親要素 → li1
-console.log(li1.textContent); // → D … Document
+const li3 = ul.lastElementChild; // 最後の子要素 → li3
+console.log(li3.textContent); // → M … Model
 
-const li2 = li1.nextElementSibling; // li1の次の兄弟要素 → li2
+const li2 = li3.previousElementSibling; // li3の前の兄弟要素 → li2
 console.log(li2.textContent); // → O … Object
 
-const li3 = li2.nextElementSibling; // li2の次の兄弟要素 → li3
-console.log(li3.textContent); // → M … Model
+const li1 = li2.previousElementSibling; // li2の前の兄弟要素 → li1
+console.log(li1.textContent); // → D … Document

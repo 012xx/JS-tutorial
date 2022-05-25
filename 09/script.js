1d795764b04501dbf44dd02b1d8e5abce22f7766
012xx <night.fox.045@gmail.com>
2022-05-25 19:39:24 +0900

    一行で書き換え

--- 09/script.js
+++ 09/script.js
@@ -3,6 +3,5 @@
 const p = document.getElementById("p_id");
 
 if (p.classList.contains("p_class") == true) {
-  p.classList.remove("p_class"); // 削除
-  p.classList.add("baseStyle", "myStyle"); // 追加
+  p.classList.replace("p_class", "baseStyle myStyle");
 }

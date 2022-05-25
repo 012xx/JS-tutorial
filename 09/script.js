0b5c12a48fe1bfe9b23debe050f9855887347e33
012xx <night.fox.045@gmail.com>
2022-05-25 15:48:12 +0900

    baseStyleクラスがclassListに含まれていなければ追加する

--- 09/script.js
+++ 09/script.js
@@ -1,15 +1,8 @@
 "use strict";
 
 const p = document.getElementById("p_id");
-console.log(p.classList);
-// → DOMTokenList ["p_class", value: "p_class"]
 
-p.className = "baseStyle"; // classNameプロパティも併用できる
-
-const flag = window.prompt("A,B, or C");
-
-if (flag == "A") {
-  p.classList.add("p_class"); // p_classを追加
-} else if (flag == "B") {
-  p.classList.add("myStyle"); // myStyleを追加
+// baseStyleクラスがclassListに含まれていなければ追加する
+if (p.classList.contains("baseStyle") == false) {
+  p.classList.add("baseStyle");
 }

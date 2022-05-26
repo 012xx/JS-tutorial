9cf5f003bb1fb8853e6c9819dd2dfd6c3bd7900b
012xx <night.fox.045@gmail.com>
2022-05-27 01:08:00 +0900

    Add:mouseleave

--- 10/script.js
+++ 10/script.js
@@ -12,3 +12,13 @@ button.addEventListener("click", () => {
   const p = document.getElementById("target");
   p.classList.toggle("newStyle");
 });
+
+button.addEventListener("mouseenter", () => {
+  const p = document.getElementById("target");
+  p.style.fontSize = "1.5em";
+});
+
+button.addEventListener("mouseleave", () => {
+  const p = document.getElementById("target");
+  p.style.fontSize = "1em";
+});

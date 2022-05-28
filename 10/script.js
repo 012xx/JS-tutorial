ea1f5c370c334f71f23d88215650c03f3c64d33c
012xx <night.fox.045@gmail.com>
2022-05-28 09:47:41 +0900

    img 要素をクリックすると画像を切り替えるように

--- 10/script.js
+++ 10/script.js
@@ -3,6 +3,8 @@
 const button = document.querySelector("button");
 console.log(button); // → <button>ボタン</button>
 
+// ボタンをクリックするとクラスが切り替わるように
+
 button.addEventListener("click", () => {
   const p = document.getElementById("target");
   p.classList.toggle("myStyle");
@@ -21,4 +23,11 @@ button.addEventListener("mouseenter", () => {
 button.addEventListener("mouseleave", () => {
   const p = document.getElementById("target");
   p.style.fontSize = "1em";
+});
+
+// img 要素をクリックすると画像を切り替えるように
+const img = document.querySelector("img");
+
+img.addEventListener("click", () => {
+  img.src = "https://github.com/ugok-girls.png";
 });
